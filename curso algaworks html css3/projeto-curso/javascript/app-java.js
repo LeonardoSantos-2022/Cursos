0bbbfe32dfde7c6de1a562528939f0eab409028a
LeonardoSantos-2022 <leonunessan@gmail.com>
2022-04-19 18:04:29 -0300

    aulas

---
+++ curso algaworks html css3/projeto-curso/javascript/app-java.js
@@ -0,0 +1,7 @@
+menuChave=document.querySelector('.botao-chaveador')
+menuChave.onclick = function() {
+classMenu=document.querySelector(".menu");
+classMenu.classList.toggle("menu-exibindo");
+}
+
+
