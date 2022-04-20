2ecee82f716f8e5f34012b8b3b3625c3a7c3b772
LeonardoSantos-2022 <leonunessan@gmail.com>
2022-04-20 17:30:23 -0300

    aulas

---
+++ curso algaworks javascript/Variáveis e hoisting/Variáveis e hoisting 2 .js
@@ -0,0 +1,12 @@
+var porcentualimpososto = 15;
+var totalimposto = valor * porcentualimpososto;
+ console.log("total do imposto a pagar",totalimposto);
+ var valor= 100 
+// HOISTING
+/*
+var percentualImposto, totalImposto, valor;
+percentualImposto = 0.15;
+totalImposto = valor * percentualImposto;
+console.log('Total imposto a pagar', totalImposto);
+valor = 100;
+*/
---
+++ curso algaworks javascript/Variáveis e hoisting/Variáveis e hoisting.js
@@ -0,0 +1,5 @@
+var porcentualimpososto = 0.15;
+var valor=100;
+var totalimposto= valor * porcentualimpososto;
+
+console.log ("total immposto apagar",totalimposto);
