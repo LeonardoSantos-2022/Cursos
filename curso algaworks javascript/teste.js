85bd5f7dab625186e16f28b9828341e92632924a
LeonardoSantos-2022 <leonunessan@gmail.com>
2022-04-29 19:08:43 -0300

    aulas

--- curso algaworks javascript/teste.js
+++ curso algaworks javascript/teste.js
@@ -1,10 +1,21 @@
-var idade = ("idade")
+var idade = (idade)
 idade = 25
 console.log(idade)
 
 var a = 29
 var b = 39
 var resultado=a*b
-console.log("resultado",resultado)
+console.log("resultado",resultado);
+
+var carros = ["celta","gol","fiat"]
+console.log(carros);
+
+var especificaçoes={
+    motor:"1.0",
+    ano:"2020",
+    fabricante:"fiat",
+    modelo:"gm",
+}
+console.log(especificaçoes);
 
 
